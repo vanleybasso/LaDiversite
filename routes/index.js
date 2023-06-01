eb82c57151c38aeb6e54d1df6df88e0b6ddbfb01
vanleybasso <vanley_basso@hotmail.com>
2023-06-01 14:44:43 -0300

    Atualizando projeto com node.js

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/whisky', function(req, res, next) {
+  res.render('categoria_whisky', { title: 'categoria_whisky' });
+});
+
+router.get('/licor', function(req, res, next) {
+  res.render('categoria_licor', { title: 'categoria_licor' });
+});
+
+router.get('/vodka', function(req, res, next) {
+  res.render('categoria_vodka', { title: 'categoria_vodka' });
+});
+
+router.get('/gin', function(req, res, next) {
+  res.render('categoria_gins', { title: 'categoria_gins' });
+});
+
+router.get('/produto', function(req, res, next) {
+  res.render('produto', { title: 'produto' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+
+module.exports = router;
